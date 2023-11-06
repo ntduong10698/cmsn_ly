8044c7260f22c520cf5c55288010d6b37d6bfd6a
forother <fortranglou@gmail.com>
2023-11-06 23:23:35 +0700

    init

---
+++ config.js
@@ -0,0 +1,23 @@
+var config = {
+    texts: [
+        "Thay mặt mọi người gửi lời chúc sinh nhật tuyệt vời nhất đến em",
+        "Ly Bé",
+        "Chúc em luôn xinh đẹp",
+        "Luôn ở bên mọi người",
+        "Luôn thành công theo cách mình muốn",
+        "Tuổi mới thành công rực rỡ"
+    ],
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+    },
+    desc: {
+        turn_on: "Bắt đầu nào",
+        play: "Thưởng thức nhạc nhé",
+        bannar_coming: "Chúc mừng sinh nhật Ly Bé",
+        balloons_flying: "Một món quà nho nhỏ",
+        cake_fadein: "Một chiếc bánh",
+        light_candle: "Thắp nến nhé",
+        wish_message: "Nhắm mắt lại và thực hiện 1 điều ước",
+        story: "A MESSAGE FOR YOU",
+    }
+};
